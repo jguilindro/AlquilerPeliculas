f83685efd681ea479f94bedd01d36bf788b3cf0c
jguilindro <julio.guilindro@gmail.com>
2016-12-26 20:43:09 -0500

    Creado CustomerTest con mockito

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,9 +1,24 @@
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
 
 public class CustomerTest {
 	@Mock
 	Customer cliente;
+	@Rule 
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
 	
-	
-	
+	 @Test
+	 public void test()  {
+		 Customer t  = spy(new Customer());
+         boolean check = t.isAdult();
+         when(check).thenReturn(true);
+ }
 }
